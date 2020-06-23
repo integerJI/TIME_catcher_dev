0b4bb93aece676420d6ab775934a81222c481b6a
integerJI <jjs9536@gmail.com>
2020-06-23 21:31:38 +0900

    한국 설정

--- app/models.py
+++ app/models.py
@@ -4,8 +4,12 @@ from django.contrib.auth.models import User
 
 class Timesave(models.Model):
     save_user = models.ForeignKey(User, on_delete = models.CASCADE)
-    save_date = models.IntegerField()
-    # save_date = models.DateTimeField('date published', null = True, default=timezone.now)
+    save_date = models.IntegerField(default=0)
+    input_date = models.DateTimeField('date published', default=timezone.now)
+    
+    # @property
+    # def total_save_time(self):
+    #     return self.save_date.count()
 
     def __str__(self):
         return '%s - %s' % (self.save_user, self.save_date) 
