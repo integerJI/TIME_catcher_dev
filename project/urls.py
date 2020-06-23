ed6c449db8d0c152930fcfc70e026f2085be0428
integerJI <jjs9536@gmail.com>
2020-06-23 20:28:20 +0900

    회원가입만들기

--- project/urls.py
+++ project/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', app.views.index, name='index'),
     path('app/', include('app.urls')),
+    path('user/', include('user.urls')),
 ]
